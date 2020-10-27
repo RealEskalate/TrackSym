12ddc373d300abada5971f8e0cbb92dd4404de96
Abela <abeltsg32@gmail.com>
2020-10-27 15:56:54 +0300

    [mobile] bloc implemented for user

--- ephi_healthcare_worker_app/lib/widgets/roundedInputField.dart
+++ ephi_healthcare_worker_app/lib/widgets/roundedInputField.dart
@@ -7,21 +7,24 @@ class RoundedInputField extends StatelessWidget {
   final String hintText;
   final IconData icon;
   final ValueChanged<String> onChanged;
+  final TextEditingController controller;
 
-  const RoundedInputField({
-    Key key,
-    this.hintText,
-    this.icon = Icons.person,
-    this.onChanged,
-    this.color,
-    this.signIn = true,
-  }) : super(key: key);
+  const RoundedInputField(
+      {Key key,
+      this.hintText,
+      this.icon = Icons.person,
+      this.onChanged,
+      this.color,
+      this.signIn = true,
+      this.controller})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextFieldContainer(
       signIn: this.signIn,
       child: TextField(
+        controller: controller,
         onChanged: onChanged,
         decoration: InputDecoration(
             icon: Icon(
