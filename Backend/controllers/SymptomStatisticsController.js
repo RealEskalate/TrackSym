99494030d39c8c58e87c5a4f63d45490aee91f8b
Infinidrix <biruksolomon11@gmail.com>
2020-11-10 12:24:47 +0300

    [api] demo support for symptom statistics

--- Backend/controllers/SymptomStatisticsController.js
+++ Backend/controllers/SymptomStatisticsController.js
@@ -1,46 +1,18 @@
-const { SymptomUser } = require("../models/SymptomUser");
-const { LocationUser } = require("../models/LocationUserModel");
-const { User } = require("../models/UserModel");
+const SymptomUserModel= require("../models/SymptomUser");
+const LocationUserModel = require("../models/LocationUserModel");
+const UserModel = require("../models/UserModel");
 const { DistrictModel } = require("../models/DistrictModel");
 const { Symptom } = require("../models/Symptom");
-const { SymptomLog } = require("../models/SymptomLogModel");
+const SymptomLogModel = require("../models/SymptomLogModel");
 const { requestWhitelist } = require("express-winston");
 const { StatisticsResource } = require("../models/StatisticsResourceModel.js");
-const { CaseInvestigation } = require("../models/CaseInvestigation");
+const CaseInvestigationModel = require("../models/CaseInvestigation");
 
 exports.get_most_common = async (req, res) => {
-    let filter = {};
-    if (req.query.date) {
-        let date = new Date(req.query.date);
-        filter.timestamp = { $gt: date };
-    }
+    let { SymptomUser } = demo_or_real_db(req.query);
 
-    if (req.query.district) {
-        let district = await DistrictModel.findOne({
-            name: req.query.district,
-        });
-        if (district) {
-            let locationUsers = await LocationUser.find({
-                "location.district": district._id,
-            }).distinct("user_id", { user_id: { $ne: null } });
-            filter.user_id = { $in: locationUsers };
-        }
-    } else if (req.query.region) {
-        let districts = await DistrictModel.find({
-            state: req.query.region,
-        }).distinct("_id", { "_id": { $ne: null } });
-        if (districts) {
-            let locationUsers = await LocationUser.find({
-                "location.district": { $in: districts },
-            }).distinct("user_id", { user_id: { $ne: null } });
-            filter.user_id = { $in: locationUsers };
-        }
-    } else if (req.query.country) {
-        let users = await User.find({
-            current_country: req.query.country,
-        }).distinct("_id", {"_id": {$ne: null}});
-        filter.user_id = { $in: users };
-    } 
+    let filter = await build_filter(req.query);
+    
     //First find appropriate documents by filter
     //Group based on distinct unique symptom ids
     //Populate/Lookup symptom values from Symptom Table
@@ -69,37 +41,11 @@ exports.get_most_common = async (req, res) => {
         },
     ]);
 
-    // // translation start
-    let language = null;
-    if (req.query.language) {
-        language = await StatisticsResource.findOne({
-            language: req.query.language,
-            title: "sypmtom-list",
-        });
-        if (language) {
-            language = language.fields[0];
-        }
-    }
-    common = []
-    for (var index = 0 in aggregated) {
-        let symptom = aggregated[index].symptom;
-        if (symptom.length == 1 && symptom[0]) {
-            symptom = symptom[0];
-            let key = symptom._id;
-            aggregated[index].symptom = aggregated[index].symptom[0];
-            if (language && language[key]) {
-                aggregated[index].symptom.name = language[key].name;
-                aggregated[index].symptom.description = language[key].description;
-                aggregated[index].symptom.relevance = language[key].relevance;
-            }
-            common.push(aggregated[index]);
-        }
-    }
-    // // translation end
+    localized_result = await translate_symptom_users(aggregated, req.query.language);
     try { 
         res.send({
-            total: common.length,
-            data: common,
+            total: localized_result.length,
+            data: localized_result,
         });
     } catch (err) {
         return res.status(500).send(err.toString());
@@ -107,33 +53,9 @@ exports.get_most_common = async (req, res) => {
 };
 
 exports.get_people_with_symptoms = async (req, res) => {
-    let filter = {};
-    if (req.query.date) {
-        let date = new Date(req.query.date);
-        filter.timestamp = { $gt: date };
-    }
-    if (req.query.district) {
-        let district = await DistrictModel.findOne({
-            name: req.query.district,
-        });
-        let locationUsers = await LocationUser.find({
-            "location.district": district._id,
-        }).distinct("user_id");
-        filter.user_id = { $in: locationUsers };
-    } else if (req.query.region) {
-        let districts = await DistrictModel.find({
-            state: req.query.region,
-        }).distinct("_id");
-        let locationUsers = await (
-            await LocationUser.find({ "location.district": { $in: districts } })
-        ).distinct("user_id");
-        filter.user_id = { $in: locationUsers };
-    } else if (req.query.country) {
-        let users = await User.find({
-            current_country: req.query.country,
-        }).distinct("_id");
-        filter.user_id = { $in: users };
-    }
+    let { SymptomUser } = demo_or_real_db(req.query);
+
+    let filter = await build_filter(req.query);
 
     let symptomUsers = await SymptomUser.find(filter).distinct("user_id");
 
@@ -145,41 +67,20 @@ exports.get_people_with_symptoms = async (req, res) => {
 };
 
 exports.get_symptom_logs_by_healthcare_worker = async (req, res) => {
+    let { CaseInvestigation, SymptomLog } = demo_or_real_db(req.query);
+    
     if (!req.params.assigned_to){
         return res.status(400).send("Healthcare worker ID not sent");
     }
 
-    let selected_ids = (await CaseInvestigation.find({
+    let filter = await build_detailed_filter(req.query);
+    let selected_user_id = (await CaseInvestigation.find({
         assigned_to: req.params.assigned_to
     })).map(investigation => investigation.user_id);
-
-    let filter = {
-        user_id: {$in: selected_ids}
-    };
-
-    if (req.query.status) {
-        filter.status = req.query.status;
-    }
-
-    if (req.query.start_date) {
-        filter["current_symptoms.date"] = {
-            $gte: new Date(req.query.start_date),
-        };
-    } else {
-        let date = new Date();
-        date.setHours(date.getHours() - 24);
-        filter["current_symptoms.date"] = { $gte: date };
-    }
-
-    if (req.query.end_date) {
-        let date = new Date(req.query.end_date);
-        date.setHours(23);
-        Object.assign(filter["current_symptoms.date"], { $lte: date });
-    }
+    filter.user_id = {$in: selected_user_id}
 
     let page = parseInt(req.query.page) || 1;
     let size = parseInt(req.query.size) || 15;
-
     let logs = await SymptomLog.find(
         filter,
         {},
@@ -196,13 +97,15 @@ exports.get_symptom_logs_by_healthcare_worker = async (req, res) => {
         })
         .sort({ "current_symptoms.date": -1 });
 
+    await translate_symptom_log(logs, req.query.language);
+    assign_risk_scores(logs);
+    
     let result = {
         data_count: await SymptomLog.countDocuments(filter),
         page_size: size,
         current_page: page,
         data: logs,
     };
-
     try {
         res.send(result);
     } catch (err) {
@@ -211,71 +114,12 @@ exports.get_symptom_logs_by_healthcare_worker = async (req, res) => {
 }
 
 exports.get_symptom_logs = async (req, res) => {
-    let filter = {
-        status: { $ne: "SYMPTOMS_REMOVED" },
-    };
+    let { SymptomLog } = demo_or_real_db(req.query);
+    let filter = await build_detailed_filter(req.query);
+    filter.status = { $ne: "SYMPTOMS_REMOVED" }
 
-    if (req.query.status) {
-        filter.status = req.query.status;
-    }
-
-    if (req.query.username) {
-        let users = await User.find({
-            username: { $regex: req.query.username, $options: "i" },
-        }).distinct("_id");
-        filter.user_id = { $in: users };
-    }
-
-    if (req.query.country) {
-        filter["current_symptoms.location.country"] = req.query.country;
-    }
-
-    if (req.query.district) {
-        let district = await DistrictModel.findOne({
-            name: req.query.district,
-        });
-        filter["current_symptoms.location.district"] = district._id;
-    }
-
-    if (req.query.region) {
-        let districts = await DistrictModel.find({
-            state: req.query.region,
-        }).distinct("_id");
-        filter["current_symptoms.location.district"] = { $in: districts };
-    }
-
-    if (req.query.start_date) {
-        filter["current_symptoms.date"] = {
-            $gte: new Date(req.query.start_date),
-        };
-    } else {
-        let date = new Date();
-        date.setHours(date.getHours() - 24);
-        filter["current_symptoms.date"] = { $gte: date };
-    }
-
-    if (req.query.end_date) {
-        let date = new Date(req.query.end_date);
-        date.setHours(23);
-        Object.assign(filter["current_symptoms.date"], { $lte: date });
-    }
-
-    if (req.query.risk_level) {
-        filter["current_symptoms.risk_score"] = {};
-        if (req.query.risk_level.toLowerCase() == "low") {
-            filter["current_symptoms.risk_score"]["$gte"] = 0;
-            filter["current_symptoms.risk_score"]["$lt"] = 0.33;
-        } else if (req.query.risk_level.toLowerCase() == "medium") {
-            filter["current_symptoms.risk_score"]["$gt"] = 0.33;
-            filter["current_symptoms.risk_score"]["$lte"] = 0.66;
-        } else if (req.query.risk_level.toLowerCase() == "high") {
-            filter["current_symptoms.risk_score"]["$gt"] = 0.66;
-            filter["current_symptoms.risk_score"]["$lte"] = 1;
-        }
-    }
     let page = parseInt(req.query.page) || 1;
     let size = parseInt(req.query.size) || 15;
-
     let logs = await SymptomLog.find(
         filter,
         {},
@@ -292,16 +136,206 @@ exports.get_symptom_logs = async (req, res) => {
         })
         .sort({ "current_symptoms.date": -1 });
 
-    // translation start
-    let language = null;
-    let genderNames = null;
-    if (req.query.language) {
+    await translate_symptom_log(logs, req.query.language);
+    assign_risk_scores(logs);
+
+    let result = {
+        data_count: await SymptomLog.countDocuments(filter),
+        page_size: size,
+        current_page: page,
+        data: logs,
+    };
+    try {
+        res.send(result);
+    } catch (err) {
+        res.status(500).send(err.toString());
+    }
+};
+
+exports.get_logs_by_user_id = async (req, res) => {
+    let { SymptomLog } = demo_or_real_db(req.query);
+    let log = await SymptomLog.findOne({ user_id: req.params.user_id });
+    if (!log) {
+        res.status(404).send("Log Not Found");
+    } else {
+        try {
+            assign_risk_scores([log]);
+            res.send(log);
+        } catch (err) {
+            res.status(500).send(err.toString());
+        }
+    }
+};
+
+
+// check if demo=true in request
+function demo_or_real_db(query) {
+    if (query.demo && query.demo == "true"){
+        return {
+            User: UserModel.DemoUser,
+            SymptomLog: SymptomLogModel.SymptomLogDemo,
+            LocationUser: LocationUserModel.DemoLocationUser,
+            CaseInvestigation: CaseInvestigationModel.CaseInvestigationDemo,
+            SymptomUser: SymptomUserModel.DemoSymptomUser
+        }
+    } else {
+        return {
+            User: UserModel.User,
+            SymptomLog: SymptomLogModel.SymptomLog,
+            LocationUser: LocationUserModel.LocationUser,
+            CaseInvestigation: CaseInvestigationModel.CaseInvestigation,
+            SymptomUser: SymptomUserModel.SymptomUser
+        }
+    }
+};
+
+// create and return a filter object based on country, district, date
+async function build_filter (query) {
+    let { User, LocationUser } = demo_or_real_db(query);
+
+    let filter = {};
+
+    if (query.date) {
+        let date = new Date(query.date);
+        filter.timestamp = { $gt: date };
+    }
+
+    if (query.district) {
+        let district = await DistrictModel.findOne({
+            name: query.district,
+        });
+        if (district) {
+            let locationUsers = await LocationUser.find({
+                "location.district": district._id,
+            }).distinct("user_id", { user_id: { $ne: null } });
+            filter.user_id = { $in: locationUsers };
+        }
+    } else if (query.region) {
+        let districts = await DistrictModel.find({
+            state: query.region,
+        }).distinct("_id", { "_id": { $ne: null } });
+        if (districts) {
+            let locationUsers = await LocationUser.find({
+                "location.district": { $in: districts },
+            }).distinct("user_id", { user_id: { $ne: null } });
+            filter.user_id = { $in: locationUsers };
+        }
+    } else if (query.country) {
+        let users = await User.find({
+            current_country: query.country,
+        }).distinct("_id", {"_id": {$ne: null}});
+        filter.user_id = { $in: users };
+    } 
+
+    return filter
+};
+
+// create object based on many params - not merged with build filter because of conflicting query naming
+async function build_detailed_filter(query) {
+    let { User } = demo_or_real_db(query);
+    let filter = {};
+
+    if (query.status) {
+        filter.status = query.status;
+    }
+
+    if (query.username) {
+        let users = await User.find({
+            username: { $regex: query.username, $options: "i" },
+        }).distinct("_id");
+        filter.user_id = { $in: users };
+    }
+
+    if (query.country) {
+        filter["current_symptoms.location.country"] = query.country;
+    }
+
+    if (query.district) {
+        let district = await DistrictModel.findOne({
+            name: query.district,
+        });
+        filter["current_symptoms.location.district"] = district._id;
+    }
+
+    if (query.region) {
+        let districts = await DistrictModel.find({
+            state: query.region,
+        }).distinct("_id");
+        filter["current_symptoms.location.district"] = { $in: districts };
+    }
+
+    if (query.start_date) {
+        filter["current_symptoms.date"] = {
+            $gte: new Date(query.start_date),
+        };
+    } else {
+        let date = new Date();
+        date.setHours(date.getHours() - 24);
+        filter["current_symptoms.date"] = { $gte: date };
+    }
+
+    if (query.end_date) {
+        let date = new Date(query.end_date);
+        date.setHours(23);
+        Object.assign(filter["current_symptoms.date"], { $lte: date });
+    }
+
+    if (query.risk_level) {
+        filter["current_symptoms.risk_score"] = {};
+        if (query.risk_level.toLowerCase() == "low") {
+            filter["current_symptoms.risk_score"]["$gte"] = 0;
+            filter["current_symptoms.risk_score"]["$lt"] = 0.33;
+        } else if (query.risk_level.toLowerCase() == "medium") {
+            filter["current_symptoms.risk_score"]["$gt"] = 0.33;
+            filter["current_symptoms.risk_score"]["$lte"] = 0.66;
+        } else if (query.risk_level.toLowerCase() == "high") {
+            filter["current_symptoms.risk_score"]["$gt"] = 0.66;
+            filter["current_symptoms.risk_score"]["$lte"] = 1;
+        }
+    }
+
+    return filter
+}
+
+// translate symptom_users to specified language
+async function translate_symptom_users(content, language) {
+    if (language) {
         language = await StatisticsResource.findOne({
-            language: req.query.language,
+            language: language,
+            title: "sypmtom-list",
+        });
+        if (language) {
+            language = language.fields[0];
+        }
+    }
+    common = []
+    for (var index = 0 in content) {
+        let symptom = content[index].symptom;
+        if (symptom.length == 1 && symptom[0]) {
+            symptom = symptom[0];
+            let key = symptom._id;
+            content[index].symptom = content[index].symptom[0];
+            if (language && language[key]) {
+                content[index].symptom.name = language[key].name;
+                content[index].symptom.description = language[key].description;
+                content[index].symptom.relevance = language[key].relevance;
+            }
+            common.push(content[index]);
+        }
+    }
+
+    return common;
+};
+
+async function translate_symptom_log(content, language) {
+    let genderNames = null;
+    if (language) {
+        language = await StatisticsResource.findOne({
+            language: language,
             title: "sypmtom-list",
         });
         genderNames = await StatisticsResource.findOne({
-            language: req.query.language,
+            language: language,
             title: "symptoms-name-list",
         });
         if (language) {
@@ -313,8 +347,8 @@ exports.get_symptom_logs = async (req, res) => {
     }
 
     if (language) {
-        for (var index = 0; index < logs.length; index++) {
-            let symptoms = logs[index].current_symptoms.symptoms;
+        for (var index = 0; index < content.length; index++) {
+            let symptoms = content[index].current_symptoms.symptoms;
             for (var symptomIndex in symptoms) {
                 let symptom = symptoms[symptomIndex];
                 let key = symptom._id;
@@ -325,12 +359,14 @@ exports.get_symptom_logs = async (req, res) => {
                 }
             }
             if (genderNames) {
-                logs[index].user_id.gender =
-                    genderNames[logs[index].user_id.gender];
+                content[index].user_id.gender =
+                    genderNames[content[index].user_id.gender];
             }
         }
     }
-    // translation end
+}
+
+function assign_risk_scores(logs){
     for (var index = 0; index < logs.length; index++) {
         let risk = logs[index].current_symptoms.risk_score || 0;
         let risk_lvl = "Low";
@@ -338,46 +374,10 @@ exports.get_symptom_logs = async (req, res) => {
             risk_lvl = "Low";
         } else if (risk <= 0.66) {
             risk_lvl = "Medium";
-        } else if (risk <= 1) {
+        } else {
             risk_lvl = "High";
         }
         logs[index].current_symptoms.risk_score = risk;
         logs[index].current_symptoms.risk_score_level = risk_lvl;
     }
-    let result = {
-        data_count: await SymptomLog.countDocuments(filter),
-        page_size: size,
-        current_page: page,
-        data: logs,
-    };
-
-    try {
-        res.send(result);
-    } catch (err) {
-        res.status(500).send(err.toString());
-    }
-};
-
-exports.get_logs_by_user_id = async (req, res) => {
-    let log = await SymptomLog.findOne({ user_id: req.params.user_id });
-    if (!log) {
-        res.status(404).send("Log Not Found");
-    } else {
-        try {
-            let risk = log.current_symptoms.risk_score || 0;
-            let risk_lvl = "Low";
-            if (risk <= 0.33) {
-                risk_lvl = "Low";
-            } else if (risk <= 0.66) {
-                risk_lvl = "Medium";
-            } else if (risk <= 1) {
-                risk_lvl = "High";
-            }
-            log.current_symptoms.risk_score = risk;
-            log.current_symptoms.risk_score_level = risk_lvl;
-            res.send(log);
-        } catch (err) {
-            res.status(500).send(err.toString());
-        }
-    }
-};
+}
